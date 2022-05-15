975e28cec6dd6158fd653306517ecac73d44b545
emptystack1024 <104486691+emptystack1024@users.noreply.github.com>
2022-05-15 10:51:43 +0800

    Update 计算平均值.c

--- simple/计算平均值.c
+++ simple/计算平均值.c
@@ -36,3 +36,24 @@ int main()
 }
 
 */
+/*while*/
+/*
+#include<stdio.h>
+int main()
+{
+	int number;
+	int sum=0;
+	int count=0;
+
+	scanf("%d",&number);
+	while(number!=-1){
+		sum+=number;
+		count++;
+		scanf("%d",&number);
+	}
+	
+	printf("平均值为%f\n",1.0*sum/count);
+	
+	return 0;
+}
+*/
