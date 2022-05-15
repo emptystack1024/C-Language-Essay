b0706f11afd8b46b372bb369075e93cf5c582c32
emptystack1024 <104486691+emptystack1024@users.noreply.github.com>
2022-05-15 11:18:24 +0800

    Create 走迷宫.c

---
+++ game/走迷宫.c
@@ -0,0 +1,69 @@
+#include<string.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <windows.h> 
+#include<conio.h>
+
+int main() { 
+	 char a[50][50]={"######", 
+ 	"#O #  ", 
+ 	"# #  #", 
+	"#   ##", 
+ 	"##   #", 
+ 	"######", 
+	}; 
+ 	int i, x, y, p, q; 
+	char ch; 
+ 	x=1; y=1; p=1; q=5;
+
+	for(i=0; i<=5; i++) 
+		puts(a[i]); 
+ 	while((x!=p)||(y!=q)) 
+ 	{ 
+ 		ch=getch(); 
+ 		if(ch=='s') 
+ 		{ 
+ 			if(a[x+1][y]!='#') 
+ 			{ 
+ 				a[x][y]=' '; 
+				x++; 
+				a[x][y]='O'; 
+ 			} 
+		} 
+ 		if(ch=='w') 
+ 		{ 
+ 			if(a[x-1][y]!='#') 
+ 			{ 
+ 				a[x][y]=' '; 
+				x--; 
+ 				a[x][y]='O'; 
+ 			} 
+ 		} 
+ 		if(ch=='a') 
+ 		{ 
+ 			if(a[x][y-1]!='#') 
+ 			{ 
+ 				a[x][y]=' '; 
+				y--; 
+ 				a[x][y]='O'; 
+ 			} 
+ 		} 
+ 		if(ch=='d')
+
+		{ 
+ 			if(a[x][y+1]!='#') 
+ 			{ 
+ 				a[x][y]=' '; 
+ 				y++; 
+ 				a[x][y]='O'; 
+ 			} 
+ 		} 
+ 		system("cls"); 
+ 		for(i=0; i<=5; i++) 
+ 			puts(a[i]); 
+ 	} 
+ 	system("cls"); 
+	printf("You win! \n"); 
+ 	Sleep(5000); 
+ 	return 0; 
+ }
