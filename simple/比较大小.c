f6a4d8297d9868d22075e46e13ad949a0c956bee
emptystack1024 <104486691+emptystack1024@users.noreply.github.com>
2022-05-15 10:04:09 +0800

    Create 比较大小.c

---
+++ simple/比较大小.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b;
+	
+	printf("请输入两个整数：");
+	scanf("%d %d",&a,&b);
+	
+	int max=0;
+	if (a>b) {
+		max=a;
+	} 	else{
+		max=b;
+	}
+	printf("大的那个是%d\n",max);
+
+return 0;
+}
