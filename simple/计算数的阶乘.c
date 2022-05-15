7c711702ffac2d9844029535328f7a42f08804f0
emptystack1024 <104486691+emptystack1024@users.noreply.github.com>
2022-05-15 10:42:55 +0800

    Create 计算数的阶乘.c

---
+++ simple/计算数的阶乘.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+
+int main(){
+	int n;
+	
+	printf("请输入要阶乘的数：");
+	scanf("%d",&n);
+	int fact=1;
+	
+	int  i=1;
+	while(i<=n){
+		fact*=i;
+		i++;
+	}
+	
+	printf("%d!=%d\n",n,fact);
+	
+	return 0;
+}
