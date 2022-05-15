c233847073383da482709d606e5ab3a3b8c67da5
emptystack1024 <104486691+emptystack1024@users.noreply.github.com>
2022-05-15 10:44:18 +0800

    Update 计算数的阶乘.c

--- simple/计算数的阶乘.c
+++ simple/计算数的阶乘.c
@@ -18,3 +18,26 @@ int main(){
 	
 	return 0;
 }
+
+
+/*for版本*/
+/*
+#include<stdio.h>
+
+int main(){
+	int n;
+	
+	printf("请输入要阶乘的数：");
+	scanf("%d",&n);
+	int fact=1;
+	
+	int  i=1;
+	for(i=1;i<=n;i++){
+		fact*=i;
+	}
+	
+	printf("%d!=%d\n",n,fact);
+	
+	return 0;
+}
+*/
